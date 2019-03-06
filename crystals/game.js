ef00a6184133d9511745bcd072d12457fe0a73a7
Daniel M <mcmanimoncoding@gmail.com>
2019-03-05 21:23:31 -0700

    init

---
+++ crystals/game.js
@@ -0,0 +1,33 @@
+// Create startup function
+// calculates a Math.random and pushes value to var targetNum
+// function displays 4 crystals var crystal1 - crystal 4
+// assigns random number value to each crystal var
+// onclick crystal will display assigned value, value will be added to var total
+
+// variables
+var targetNum = "0";
+var yourNum = "0";
+var wins = 0;
+var losses = 0;
+
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
+
+$(document).ready(function () {
+
+    function startUp() {
+
+    $("#your-total-display").text(yourNum);
+    $("#target-display").text(targetNum);
+    $("#wins-display").text(wins);
+    $("#losses-display").text(losses);
+
+
+    };
+
+    startUp();
+
+});
+
